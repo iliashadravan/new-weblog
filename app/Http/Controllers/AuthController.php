9a7378d05c01e9978b033f34bc216074d3f8c502
Gcc <iliashadravan84@gmail.com>
2025-02-10 13:24:40 +0330

    Add Auth

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,46 +2,46 @@
 
 namespace App\Http\Controllers;
 
-use App\Classes\ApiResponse;
-use App\Http\Requests\AuthController\LoginRequest;
-use App\Http\Requests\AuthController\RegisterRequest;
 use App\Models\User;
-use Illuminate\Auth\Events\Login;
+use App\Http\Requests\AuthController\RegisterRequest;
+use App\Http\Requests\AuthController\LoginRequest;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        $request->validated();
-
         $user = User::create([
-            'firstname' => $request->input('firstname'),
-            'lastname'  => $request->input('lastname'),
-            'email'     => $request->input('email'),
-            'phone'     => $request->input('phone'),
-            'password'  => Hash::make($request->input('password')),
+            'firstname' => $request->firstname,
+            'lastname'  => $request->lastname,
+            'email'     => $request->email,
+            'phone'     => $request->phone,
+            'password'  => Hash::make($request->password),
         ]);
 
-        $token = JWTAuth::fromUser($user);
-
-        return ApiResponse::created([
-            'user' => $user,
-            'access_token' => $token
-        ]);
+        return response()->json([
+            'user' => $user
+        ], 201);
     }
+
     public function login(LoginRequest $request)
     {
-        $credentials = $request->only(['email', 'password']);
+        $credentials = $request->only('email', 'password');
 
         if (!$token = JWTAuth::attempt($credentials)) {
-            return ApiResponse::unauthorized();
+            return response()->json();
         }
 
-        return ApiResponse::send([
-            'user' => auth()->user(),
-            'access_token' => $token
+        return response()->json([
+            'user' => Auth::user(),
+            'token' => $token
         ]);
+    }
+
+    public function me()
+    {
+        return response()->json(Auth::user());
     }
 }
