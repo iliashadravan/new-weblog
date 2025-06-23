054176445ab6f6ca8efeeca8dc900bebec51ee57
iliashadravan <iliashadravan84@gmail.com>
2025-06-23 17:19:20 +0330

    test4

--- app/Http/Requests/ArticleController/updateRequest.php
+++ app/Http/Requests/ArticleController/updateRequest.php
@@ -16,7 +16,7 @@ class updateRequest extends Request
     public function rules(): array
     {
         return [
-            'title'       => 'required|min:3|max:51',
+            'title'       => 'required|min:3|max:50',
             'body'        => 'required',
             'categories'  => 'nullable|array',
             'categories.*'=> 'exists:categories,id',
